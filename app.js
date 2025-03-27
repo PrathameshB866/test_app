bfea8c6cb8225424b39a0896963008f5e5235e05
Prathamesh <prathameshb@heaptrace.com>
2025-03-27 13:39:01 +0530

    updated message and added 3 more lines

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 console.log("hello");
 console.log("welcome to git ");
 console.log("first commit ");
+
+//editing the file 
+
+console.log("adding more lines");
+console.log("created new branch ");
+console.log("this is a new change ");
